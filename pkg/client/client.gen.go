b6d2b9ff20c9d6734c31c7987de098704d6adb7b
Gloria Ciavarrini <gciavarrini@redhat.com>
2026-01-16 09:55:36 +0100

    Fix typos in client config and openapi spec
    Wrong module path in client.gen.cfg and API version typo in openapi.yaml.
    
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>

--- pkg/client/client.gen.go
+++ pkg/client/client.gen.go
@@ -13,7 +13,7 @@ import (
 	"net/url"
 	"strings"
 
-	. "github.com/dcm-project/service-provider-api/api/v1alpha1"
+	. "github.com/dcm-project/service-provider-manager/api/v1alpha1"
 	"github.com/oapi-codegen/runtime"
 	openapi_types "github.com/oapi-codegen/runtime/types"
 )
